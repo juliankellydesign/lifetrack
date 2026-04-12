c47a0b3241b6066f1cb9727acee256385ffbab54
Julian Kelly <julianfkelly@gmail.com>
2026-04-11 22:45:51 -0400

    Initial Commit

---
+++ lifetrack/lifetrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifetrackApp.swift
+//  lifetrack
+//
+//  Created by Julian Kelly on 4/11/26.
+//
+
+import SwiftUI
+
+@main
+struct lifetrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
